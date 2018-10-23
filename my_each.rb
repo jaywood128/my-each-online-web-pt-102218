96af918998a081b8403057ee2474a656ab9c1237
Johnathon Wood <jaywood128@gmail.com>
2018-10-23 18:42:07 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(words)
+  if block_given?
+  i = 0 
+  while i < word.length
+    yield words[i]
+  end
+
+  words
+  else
+    "No block given!" # put argument(s) here
   # code here
 end
